6d9a2a041942cfd86fb6fd1ac032e3821e983561
MikeHeb <michael.hebert1@maine.edu>
2015-10-19 20:36:45 -0400

    BlackBoard feature test

--- app/src/main/java/edu/uma/umasimple/MainActivity.java
+++ app/src/main/java/edu/uma/umasimple/MainActivity.java
@@ -6,8 +6,11 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spannable;
@@ -32,6 +35,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -106,6 +110,12 @@ public class MainActivity extends Activity {
         	 getCourseSelectForm();
          }
      });
+     tabHost.getTabWidget().getChildAt(2).setOnClickListener(new OnClickListener() {
+        public void onClick(View v) {
+			getBlackBoard();
+
+		}
+     });
      tabHost.getTabWidget().getChildAt(1).setOnClickListener(new OnClickListener() {
          public void onClick(View v) {
         	 
@@ -153,6 +163,35 @@ public class MainActivity extends Activity {
      	
      		
     
+	}
+	void getBlackBoard(){
+
+		try {
+			PackageManager packTest = getPackageManager();	//Gets the package to check for app
+			Intent openBlack = getPackageManager().getLaunchIntentForPackage("com.blackboard.android");  // this builds the intent with the app name.
+			List<ResolveInfo> activities = packTest.queryIntentActivities(openBlack, 0);  // adds the list of results
+			Boolean isSafe = activities.size() > 0;  //is true if over one
+			if (isSafe) {
+				startActivity(openBlack);  // if true then starts it
+			}
+		}
+		catch (Exception e) {
+
+		//Currently not working need to check it more
+            PackageManager packTest = getPackageManager();	//Gets the package to check for app
+			Intent openPlay = new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=Blackboard"));
+			List<ResolveInfo> activities = packTest.queryIntentActivities(openPlay, 0);
+			Boolean isSafe = activities.size() > 0;
+			if (isSafe) {
+				startActivity(openPlay);
+			}
+		}
+        finally {
+            Intent bbWebIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.courses.maine.edu/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1"));
+            startActivity(bbWebIntent);
+		}
+
+
 	}
 	void getLogin() 
 	{
